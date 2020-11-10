2b546d7c8f6292eed82628c6612a77bded416bc3
m18018230731 <m18018230731@163.com>
2020-11-10 08:15:00 +0800

    修正

--- 一键安装nginx反代steam 并安装socks和http代理/oneclick.sh
+++ 一键安装nginx反代steam 并安装socks和http代理/oneclick.sh
@@ -8,16 +8,16 @@ firewall-cmd --add-port=18779/tcp --permanent
 firewall-cmd --add-port=15055/tcp --permanent
 firewall-cmd --reload  
 
-yum install epel-release
-yum -y install wget curl
+yum install epel-release -y
+# yum -y install wget curl -y
 # nginx
-yum install nginx
+yum install nginx -y
 wget https://raw.githubusercontent.com/m18018230731/kantan-tools/master/%E4%B8%80%E9%94%AE%E5%AE%89%E8%A3%85nginx%E5%8F%8D%E4%BB%A3steam%20%E5%B9%B6%E5%AE%89%E8%A3%85socks%E5%92%8Chttp%E4%BB%A3%E7%90%86/nginx.conf
 mv nginx.conf /etc/nginx/ -y
 nginx -s reload
 service nginx start
 # tinyproxy
-yum -y install tinyproxy
+yum -y install tinyproxy -y
 wget https://raw.githubusercontent.com/m18018230731/kantan-tools/master/%E4%B8%80%E9%94%AE%E5%AE%89%E8%A3%85nginx%E5%8F%8D%E4%BB%A3steam%20%E5%B9%B6%E5%AE%89%E8%A3%85socks%E5%92%8Chttp%E4%BB%A3%E7%90%86/tinyproxy.conf
 mv tinyproxy.conf /etc/tinyproxy/ -y
 systemctl start tinyproxy.service
