c8791220f35af82e01720bf68c30c6f5ee4c3d0d
m18018230731 <m18018230731@163.com>
2020-11-10 08:05:30 +0800

    一脚脚本完成

--- 一键安装nginx反代steam 并安装socks和http代理/oneclick.sh
+++ 一键安装nginx反代steam 并安装socks和http代理/oneclick.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
 
+# nginx
+firewall-cmd --add-port=80/tcp --permanent
+# http
+firewall-cmd --add-port=18779/tcp --permanent
+# socks5
+firewall-cmd --add-port=15055/tcp --permanent  
+
 yum install epel-release
+yum -y install wget curl
+# nginx
 yum install nginx
-wget 
+wget https://raw.githubusercontent.com/m18018230731/kantan-tools/master/%E4%B8%80%E9%94%AE%E5%AE%89%E8%A3%85nginx%E5%8F%8D%E4%BB%A3steam%20%E5%B9%B6%E5%AE%89%E8%A3%85socks%E5%92%8Chttp%E4%BB%A3%E7%90%86/nginx.conf
+mv nginx.conf /etc/nginx/ -y
+nginx -s reload
+service nginx start
+# tinyproxy
+yum -y install tinyproxy
+mv tinyproxy.conf /etc/tinyproxy/ -y
+systemctl start tinyproxy.service
+# socks5
+wget https://raw.githubusercontent.com/qinghuas/socks5-install/master/socks5.sh;bash socks5.sh
+socks5 install -y
+socks5 user add rabproxy 12z991
+socks5 start
+# completed
+echo "Completed!"
