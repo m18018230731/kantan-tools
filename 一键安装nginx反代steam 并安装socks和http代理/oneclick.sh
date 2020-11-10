31547ef3104b4551c85bd2edc1c06a840b3abd4f
m18018230731 <m18018230731@163.com>
2020-11-10 08:18:49 +0800

    修正

--- 一键安装nginx反代steam 并安装socks和http代理/oneclick.sh
+++ 一键安装nginx反代steam 并安装socks和http代理/oneclick.sh
@@ -8,22 +8,23 @@ firewall-cmd --add-port=18779/tcp --permanent
 firewall-cmd --add-port=15055/tcp --permanent
 firewall-cmd --reload  
 
-yum install epel-release -y
+yum -y update
+yum -y install epel-release
 # yum -y install wget curl -y
 # nginx
-yum install nginx -y
+yum -y install nginx
 wget https://raw.githubusercontent.com/m18018230731/kantan-tools/master/%E4%B8%80%E9%94%AE%E5%AE%89%E8%A3%85nginx%E5%8F%8D%E4%BB%A3steam%20%E5%B9%B6%E5%AE%89%E8%A3%85socks%E5%92%8Chttp%E4%BB%A3%E7%90%86/nginx.conf
-mv nginx.conf /etc/nginx/ -y
+mv -f nginx.conf /etc/nginx/nginx.conf
 nginx -s reload
 service nginx start
 # tinyproxy
-yum -y install tinyproxy -y
+yum -y install tinyproxy
 wget https://raw.githubusercontent.com/m18018230731/kantan-tools/master/%E4%B8%80%E9%94%AE%E5%AE%89%E8%A3%85nginx%E5%8F%8D%E4%BB%A3steam%20%E5%B9%B6%E5%AE%89%E8%A3%85socks%E5%92%8Chttp%E4%BB%A3%E7%90%86/tinyproxy.conf
-mv tinyproxy.conf /etc/tinyproxy/ -y
+mv -f tinyproxy.conf /etc/tinyproxy/tinyproxy.conf
 systemctl start tinyproxy.service
 # socks5
 wget https://raw.githubusercontent.com/qinghuas/socks5-install/master/socks5.sh;bash socks5.sh
-socks5 install -y
+socks5 install
 socks5 user add rabproxy 12z991
 socks5 start
 # completed
