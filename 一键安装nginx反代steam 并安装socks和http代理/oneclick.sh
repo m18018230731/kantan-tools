26d953944eea766c96561d30ab5cd430654ef011
m18018230731 <m18018230731@163.com>
2020-11-10 08:06:43 +0800

    一脚脚本修正

--- 一键安装nginx反代steam 并安装socks和http代理/oneclick.sh
+++ 一键安装nginx反代steam 并安装socks和http代理/oneclick.sh
@@ -5,7 +5,8 @@ firewall-cmd --add-port=80/tcp --permanent
 # http
 firewall-cmd --add-port=18779/tcp --permanent
 # socks5
-firewall-cmd --add-port=15055/tcp --permanent  
+firewall-cmd --add-port=15055/tcp --permanent
+firewall-cmd --reload  
 
 yum install epel-release
 yum -y install wget curl
@@ -17,6 +18,7 @@ nginx -s reload
 service nginx start
 # tinyproxy
 yum -y install tinyproxy
+wget https://raw.githubusercontent.com/m18018230731/kantan-tools/master/%E4%B8%80%E9%94%AE%E5%AE%89%E8%A3%85nginx%E5%8F%8D%E4%BB%A3steam%20%E5%B9%B6%E5%AE%89%E8%A3%85socks%E5%92%8Chttp%E4%BB%A3%E7%90%86/tinyproxy.conf
 mv tinyproxy.conf /etc/tinyproxy/ -y
 systemctl start tinyproxy.service
 # socks5
