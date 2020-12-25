c38e80a28cc3ce9b73da7bdab4625a757a0cf74d
1289200420@qq.com <rabbir_bot_01>
2020-12-25 17:38:06 +0800

    按照 PEP8 规范代码，修改脚本

--- 下载页面内容/main.py
+++ 下载页面内容/main.py
@@ -1,44 +1,46 @@
 #!/usr/bin/env python
-#-*- coding:utf-8 -*
+# -*- coding:UTF-8 -*-
 
-import requests
+
 import re
+import requests
 from bs4 import BeautifulSoup
 
-def saveAsHtml(htmlContent):
+
+def save_as_html(html_content):
     try:
-        f = open("page.html",mode="w",encoding="utf-8")
-        htmlContent = BeautifulSoup(htmlContent,"html.parser").prettify()
-        f.write(str(htmlContent))
+        f = open("page.html", mode="w",encoding="utf-8")
+        html_content = BeautifulSoup(html_content, "html.parser").prettify()
+        f.write(str(html_content))
         f.close()
     except Exception as e:
         print(e)
 
-
-def getUrl():
+def get_url():
     # input the url
-    inputStr = input("The url:")
-    if("http" not in inputStr):
-        inputStr = "https://" + inputStr
-    url = re.findall('https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+', inputStr)
+    input_str = input("The url:")
+    if ("http" not in input_str):
+        input_str = "https://" + input_str
+    url = re.findall('https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+', input_str)
     return url[0]
 
-def getHtmlContent(url):
+def get_html_content(url):
     try:
-        r = requests.get(url,timeout=5)
-        if(r.status_code == 200):
+        r = requests.get(url, timeout=5)
+        r.encoding = "UTF-8"
+        if (r.status_code == 200):
             return r.text
         else:
             return ""
     except Exception as e:
         print(e)
 
-
+# main
 if __name__ == "__main__":
-    url = getUrl()
-    if(url != ""):
-        htmlContent = getHtmlContent(url)
-        saveAsHtml(htmlContent)
+    url = get_url()
+    if (url != ""):
+        html_content = get_html_content(url)
+        save_as_html(html_content)
     else:
         print("The URL is not reasonable!")
 
--- 生成指定大小的文件/main.py
+++ 生成指定大小的文件/main.py
@@ -1,48 +1,40 @@
 #!/usr/bin/env python
-#-*- coding:utf-8 -*-
+# -*- coding:UTF-8 -*-
+
 
 import os
 import random
 
-def genSizeFile(fileName, fileSize):
-    #file path
-    filePath=fileName
-    # ========== mode1 =========
-    # date size
-    # ds=0
-    # with open(filePath, "w", encoding="utf8") as f:
-    # while ds<fileSize:
-    # f.write(str(round(random.uniform(-1000, 1000),2)))
-    # f.write("\n")
-    # ds=os.path.getsize(filePath)
-    # print(os.path.getsize(filePath))
-    # ========== mode2 =========
-    file = open(filePath,'w')
-    file.seek(fileSize)
+
+def gen_size_file(file_name, file_size):
+    # file path
+    file_path = file_name
+    file = open(file_path, 'w')
+    file.seek(file_size)
     file.write('\x00')
     file.close()
     print("done!")
 
-def getFileSize():
-    # start here.
-    fileName = input("The file name you need:")
-    fileSize = input("The size you need:").lower().replace("b","")
+def get_file_size():
+    # start here
+    file_name = input("The file name you need:")
+    file_size = input("The size you need:").lower().replace("b", "")
     # size
-    if("k" in fileSize):
-        fileSize = int(fileSize.replace("k",""))*1024
-    elif("m" in fileSize):
-        fileSize = int(fileSize.replace("m",""))*1024*1024
-    elif("g" in fileSize):
-        fileSize = int(fileSize.replace("g",""))*1024*1024*1024
+    if ("k" in file_size):
+        file_size = int(file_size.replace("k", ""))*1024
+    elif("m" in file_size):
+        file_size = int(file_size.replace("m",""))*1024*1024
+    elif("g" in file_size):
+        file_size = int(file_size.replace("g",""))*1024*1024*1024
     else:
-        fileSize = int(fileSize)*1024
-    return fileName,fileSize
+        file_size = int(file_size)*1024
+    return file_name, file_size
 
-
+# main
 if __name__ == "__main__":
-    fileName,fileSize = getFileSize()
+    file_name, file_size = get_file_size()
     try:
-        print("FileSize:"+str(fileSize)+"kb!building...")
-        genSizeFile(fileName,fileSize)
+        print("FileSize:"+str(file_size)+"kb!building...")
+        gen_size_file(file_name, file_size)
     except Exception as e:
         print(e)
