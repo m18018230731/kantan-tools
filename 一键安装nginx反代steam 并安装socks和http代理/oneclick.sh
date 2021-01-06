fb61e78c55f71c92a5dc11d2de1bd0cb13cd87be
Rabbir <m18018230731@163.com>
2021-01-06 17:25:29 +0800

    新增是否安装 Socks5 的判断

--- 一键安装nginx反代steam 并安装socks和http代理/oneclick.sh
+++ 一键安装nginx反代steam 并安装socks和http代理/oneclick.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# 判断是否需要安装 Socks5 代理
+read -p "是否需要安装 Socks5 代理（0：否 1：是）：" socks5_flg
+
 # Nginx
 firewall-cmd --add-port=80/tcp --permanent
 # HTTP
@@ -11,6 +14,7 @@ firewall-cmd --reload
 yum -y update
 yum -y install epel-release
 # yum -y install wget curl -y
+
 # Nginx
 yum -y install nginx
 wget https://raw.githubusercontent.com/m18018230731/kantan-tools/master/%E4%B8%80%E9%94%AE%E5%AE%89%E8%A3%85nginx%E5%8F%8D%E4%BB%A3steam%20%E5%B9%B6%E5%AE%89%E8%A3%85socks%E5%92%8Chttp%E4%BB%A3%E7%90%86/nginx.conf
@@ -19,18 +23,22 @@ chmod 777 /etc/nginx/nginx.conf
 nginx -s reload
 service nginx start
 systemctl enable nginx.service
+
 # Tinyproxy
 yum -y install tinyproxy
 wget https://raw.githubusercontent.com/m18018230731/kantan-tools/master/%E4%B8%80%E9%94%AE%E5%AE%89%E8%A3%85nginx%E5%8F%8D%E4%BB%A3steam%20%E5%B9%B6%E5%AE%89%E8%A3%85socks%E5%92%8Chttp%E4%BB%A3%E7%90%86/tinyproxy.conf
 mv -f tinyproxy.conf /etc/tinyproxy/tinyproxy.conf
 systemctl start tinyproxy.service
 systemctl enable tinyproxy.service
+
 # Socks5
-wget https://raw.githubusercontent.com/qinghuas/socks5-install/master/socks5.sh;bash socks5.sh
-socks5 install
-socks5 user add rabproxy 12z991
-wget https://raw.githubusercontent.com/m18018230731/kantan-tools/master/%E4%B8%80%E9%94%AE%E5%AE%89%E8%A3%85nginx%E5%8F%8D%E4%BB%A3steam%20%E5%B9%B6%E5%AE%89%E8%A3%85socks%E5%92%8Chttp%E4%BB%A3%E7%90%86/ss5
-mv -f ss5 /etc/init.d/ss5
-socks5 start
+if test ${socks5_flg} -gt 0
+  wget https://raw.githubusercontent.com/qinghuas/socks5-install/master/socks5.sh;bash socks5.sh
+  socks5 install
+  socks5 user add rabproxy 12z991
+  wget https://raw.githubusercontent.com/m18018230731/kantan-tools/master/%E4%B8%80%E9%94%AE%E5%AE%89%E8%A3%85nginx%E5%8F%8D%E4%BB%A3steam%20%E5%B9%B6%E5%AE%89%E8%A3%85socks%E5%92%8Chttp%E4%BB%A3%E7%90%86/ss5
+  mv -f ss5 /etc/init.d/ss5
+  socks5 start
+
 # Completed
 echo "Completed!"
