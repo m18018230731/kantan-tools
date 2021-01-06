01014fdf533125ccebdaecc3a979780e0639796c
Rabbir <m18018230731@163.com>
2021-01-06 17:51:29 +0800

    新增 Nginx 绑定域名并阻止 IP、

--- 一键安装nginx反代steam 并安装socks和http代理/oneclick.sh
+++ 一键安装nginx反代steam 并安装socks和http代理/oneclick.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+# 自定义域名
+read -p "输入需要绑定的域名（不填则为不绑定域名）：" domain
 # 判断是否需要安装 Socks5 代理
-read -p "是否需要安装 Socks5 代理（0：否 1：是）：" socks5_flg
+read -p "是否需要安装 Socks5 代理（0：否 1：是；默认为否）：" socks5_flg
 
 # Nginx
 firewall-cmd --add-port=80/tcp --permanent
@@ -17,7 +19,13 @@ yum -y install epel-release
 
 # Nginx
 yum -y install nginx
-wget https://raw.githubusercontent.com/m18018230731/kantan-tools/master/%E4%B8%80%E9%94%AE%E5%AE%89%E8%A3%85nginx%E5%8F%8D%E4%BB%A3steam%20%E5%B9%B6%E5%AE%89%E8%A3%85socks%E5%92%8Chttp%E4%BB%A3%E7%90%86/nginx.conf
+# 如果不需要绑定域名
+if [! -n ${domain}]; then
+  wget https://raw.githubusercontent.com/m18018230731/kantan-tools/master/%E4%B8%80%E9%94%AE%E5%AE%89%E8%A3%85nginx%E5%8F%8D%E4%BB%A3steam%20%E5%B9%B6%E5%AE%89%E8%A3%85socks%E5%92%8Chttp%E4%BB%A3%E7%90%86/nginx_ip.conf -O nginx.conf
+else
+  wget https://raw.githubusercontent.com/m18018230731/kantan-tools/master/%E4%B8%80%E9%94%AE%E5%AE%89%E8%A3%85nginx%E5%8F%8D%E4%BB%A3steam%20%E5%B9%B6%E5%AE%89%E8%A3%85socks%E5%92%8Chttp%E4%BB%A3%E7%90%86/nginx_domain.conf -O nginx.conf
+  sed -i 's|your_domain|${domain}' nginx.conf
+fi
 mv -f nginx.conf /etc/nginx/nginx.conf
 chmod 777 /etc/nginx/nginx.conf
 nginx -s reload
@@ -32,13 +40,14 @@ systemctl start tinyproxy.service
 systemctl enable tinyproxy.service
 
 # Socks5
-if test ${socks5_flg} -gt 0
+if [-n ${socks5_flg}] || [test ${socks5_flg} -gt 0]; then
   wget https://raw.githubusercontent.com/qinghuas/socks5-install/master/socks5.sh;bash socks5.sh
   socks5 install
   socks5 user add rabproxy 12z991
   wget https://raw.githubusercontent.com/m18018230731/kantan-tools/master/%E4%B8%80%E9%94%AE%E5%AE%89%E8%A3%85nginx%E5%8F%8D%E4%BB%A3steam%20%E5%B9%B6%E5%AE%89%E8%A3%85socks%E5%92%8Chttp%E4%BB%A3%E7%90%86/ss5
   mv -f ss5 /etc/init.d/ss5
   socks5 start
+fi
 
 # Completed
 echo "Completed!"
