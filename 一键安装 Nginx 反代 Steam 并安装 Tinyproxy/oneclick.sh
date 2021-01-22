cd7705edb0e9cbc819ef401a8ce2f1ccba300367
Rabbir <m18018230731@163.com>
2021-01-22 22:32:40 +0800

    更新 oneclick.sh 脚本中的配置文件地址

--- 一键安装 Nginx 反代 Steam 并安装 Tinyproxy/oneclick.sh
+++ 一键安装 Nginx 反代 Steam 并安装 Tinyproxy/oneclick.sh
@@ -3,10 +3,10 @@
 # 参数读取
 if [ "$1" = "" ] || [ $1 = 0 ]; then
   echo "不绑定域名"
-  wget https://raw.githubusercontent.com/m18018230731/kantan-tools/master/%E4%B8%80%E9%94%AE%E5%AE%89%E8%A3%85nginx%E5%8F%8D%E4%BB%A3steam%20%E5%B9%B6%E5%AE%89%E8%A3%85socks%E5%92%8Chttp%E4%BB%A3%E7%90%86/nginx_ip.conf -O nginx.conf
+  wget https://raw.githubusercontent.com/senjianlu/kantan-tools/master/%E4%B8%80%E9%94%AE%E5%AE%89%E8%A3%85%20Nginx%20%E5%8F%8D%E4%BB%A3%20Steam%20%E5%B9%B6%E5%AE%89%E8%A3%85%20Tinyproxy/nginx_ip.conf -O nginx.conf
 else
   echo "绑定域名：$1"
-  wget https://raw.githubusercontent.com/m18018230731/kantan-tools/master/%E4%B8%80%E9%94%AE%E5%AE%89%E8%A3%85nginx%E5%8F%8D%E4%BB%A3steam%20%E5%B9%B6%E5%AE%89%E8%A3%85socks%E5%92%8Chttp%E4%BB%A3%E7%90%86/nginx_domain.conf -O nginx.conf
+  wget https://raw.githubusercontent.com/senjianlu/kantan-tools/master/%E4%B8%80%E9%94%AE%E5%AE%89%E8%A3%85%20Nginx%20%E5%8F%8D%E4%BB%A3%20Steam%20%E5%B9%B6%E5%AE%89%E8%A3%85%20Tinyproxy/nginx_domain.conf -O nginx.conf
   sed -i "s/your_domain/$1/" nginx.conf
 fi
 if [ "$2" = "" ] || [ $2 = 0 ]; then
@@ -51,11 +51,11 @@ make &&
 make install
 cd ..
 # 下载配置文件
-wget https://raw.githubusercontent.com/m18018230731/kantan-tools/master/%E4%B8%80%E9%94%AE%E5%AE%89%E8%A3%85nginx%E5%8F%8D%E4%BB%A3steam%20%E5%B9%B6%E5%AE%89%E8%A3%85socks%E5%92%8Chttp%E4%BB%A3%E7%90%86/tinyproxy_1.11.conf -O tinyproxy.conf
+wget https://raw.githubusercontent.com/senjianlu/kantan-tools/master/%E4%B8%80%E9%94%AE%E5%AE%89%E8%A3%85%20Nginx%20%E5%8F%8D%E4%BB%A3%20Steam%20%E5%B9%B6%E5%AE%89%E8%A3%85%20Tinyproxy/tinyproxy_1.11.conf -O tinyproxy.conf
 mkdir /etc/tinyproxy
 mv tinyproxy.conf /etc/tinyproxy/tinyproxy.conf
 # 自动启动
-wget https://raw.githubusercontent.com/m18018230731/kantan-tools/master/%E4%B8%80%E9%94%AE%E5%AE%89%E8%A3%85nginx%E5%8F%8D%E4%BB%A3steam%20%E5%B9%B6%E5%AE%89%E8%A3%85socks%E5%92%8Chttp%E4%BB%A3%E7%90%86/tinyproxy.sh -O tinyproxy.sh
+wget https://raw.githubusercontent.com/senjianlu/kantan-tools/master/%E4%B8%80%E9%94%AE%E5%AE%89%E8%A3%85%20Nginx%20%E5%8F%8D%E4%BB%A3%20Steam%20%E5%B9%B6%E5%AE%89%E8%A3%85%20Tinyproxy/tinyproxy.sh -O tinyproxy.sh
 chmod +x tinyproxy.sh
 sed -i '$a su - root -c "/root/tinyproxy.sh"' /etc/rc.d/rc.local
 chmod +x /etc/rc.d/rc.local
