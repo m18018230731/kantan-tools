36ad7714dfe69b40caf1ea6ccf28fef8fe916f33
senjianlu <m18018230731@163.com>
2021-03-14 13:20:22 +0800

    新增 PDF 转储图片过程中的图片切割功能

--- 将每页 PDF 转为图片/main.py
+++ 将每页 PDF 转为图片/main.py
@@ -11,6 +11,7 @@
 
 import sys
 import fitz
+from PIL import Image
 
 
 """
@@ -24,6 +25,30 @@ def get_pdf_file(pdf_file_path):
     return fitz.open(pdf_file_path)
 
 """
+@description: 将图片切割为左右两份
+-------
+@param:
+-------
+@return:
+"""
+def cut_image(image, image_file_name, count):
+    # 创建切割后图片存储用列表
+    width, height = image.size
+    item_width = int(width/count)
+    item_height = height
+    box_list = []
+    for i in range(0, count):
+        box = (i*item_width, 0, (i+1)*item_width, item_height)
+        box_list.append(box)
+    # 图片切割
+    image_list = [image.crop(box) for box in box_list]
+    # 保存图片
+    for image_index, image in enumerate(image_list):
+        
+        image.save("图片/切割后/" + image_file_name + "-" + str(image_index) \
+                    + ".png", "PNG")
+
+"""
 @description: 单体测试
 -------
 @param:
@@ -31,6 +56,7 @@ def get_pdf_file(pdf_file_path):
 @return:
 """
 if __name__ == "__main__":
+    # PDF 每页转储为图片
     pdf_file_name = sys.argv[1]
     if (".pdf" not in pdf_file_name):
         pdf_file_name = pdf_file_name + ".pdf"
@@ -41,8 +67,11 @@ if __name__ == "__main__":
             page = pdf_file.loadPage(page_no)
             pix = page.getPixmap()
             pix.writePNG("图片/" + str(page_no) + ".png")
+            # 图片从中间横向切割为 2 份
+            cut_image(
+                Image.open("图片/" + str(page_no) + ".png"), str(page_no), 2)
     except Exception as e:
-        print("出错：" + str(e))
+        print("PDF 每页转储为图片并切割时出错：" + str(e))
     finally:
         pdf_file.close()
-    print("完成！ ")
+    print("PDF 每页转储为图片并切割完成！ ")
